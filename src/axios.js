2691448ff3aa5193d233c1dc77af3ce7edf97248
TyGG <879705054@qq.com>
2024-11-12 22:01:50 +0800

    修改了bug

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://39.105.108.111:3000', // 指定端口号
-  timeout: 10000 // 可选: 设置请求超时
+  baseURL: 'http://39.105.108.111' // 指定端口号
+  // timeout: 10000 // 可选: 设置请求超时
 })
 
 export default apiClient
